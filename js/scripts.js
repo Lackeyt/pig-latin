be59607dce1e33d2bf3b964c4c9361513a28c98e
Tyson Lackey <lackeyt90@gmail.com>
2020-06-10 14:16:49 -0700

    added vowel index, remove letter array
    co-authored-by: Thomas Glenn <thomasgrantglenn@mac.com>

--- js/scripts.js
+++ js/scripts.js
@@ -6,14 +6,15 @@ $(document).ready(function() {
     return string.split(" ");
   }
 
-  // Split each element of an array of words into an array of letters
-  function letterArray(array) {
-    for (i=0; i < array.length; i++){
-      array[i].split("");
+  // index of first Vowel
+  function firstVowelIndex(string){
+    for (i=0;i<string.length;i++){
+      letter = string.charAt(i);
+      if(isVowel(letter, vowels)){ 
+        return i;
+      };
     };
-    return array
   };
-  
 
   // check if first character of an Array is a vowel: return boolean
   function isVowel(array, vowels){
@@ -30,7 +31,7 @@ $(document).ready(function() {
   // Add "way" to the end of an Array
   function firstVowelMove(array){
     return array.join("") + "way"
-  }
+  };
 
   // Move first letter of a string to the end, then append "ay"
   function firstConsMove(array){
@@ -38,7 +39,19 @@ $(document).ready(function() {
     return array.slice(1,(array.length + 1)).join("") + "ay";
   };
 
+
+
+
+
+
+
+
+
+
+
   //User Interface
+  
+
   $("#inputForm").submit(function(event) {
     event.preventDefault();
 
